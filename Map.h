8e10cb972824361a4303fba77556e8da238c03ca
Марина Карась <marina2006.karas@yandex.ru>
2024-07-08 20:22:17 +0300

    few new files

---
+++ Map.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+using namespace sf;
+
+
+const int H = 12;
+const int W = 40;
+
+//map
+String TileMap[H] = {
+
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+"B                                B     B",
+"B                                B     B",
+"B                                B     B",
+"B                                B     B",
+"B         0000                BBBB     B",
+"B                                B     B",
+"BBB                              B     B",
+"B              BB                BB    B",
+"B              BB                      B",
+"B    B         BB         BB           B",
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+
+};
---
+++ Player.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Map.h"
+
+using namespace sf;
+
+float offsetX = 0, offsetY = 0;
+
+class PLAYER {
+
+public:
+
+	float dx, dy;
+	FloatRect rect;
+	bool onGround;
+	Sprite sprite;
+	float currentFrame;
+
+	PLAYER(Texture& image)
+	{
+		sprite.setTexture(image);
+		rect = FloatRect(320, 240, 60, 60);
+
+		dx = dy = 0.1;
+		currentFrame = 0;
+	}
+
+
+	void update(float time)
+	{
+
+		rect.left += dx * time;
+		Collision(0);
+
+		if (!onGround) dy = dy + 0.0005 * time;
+		rect.top += dy * time;
+		onGround = false;
+		Collision(1);
+
+
+		currentFrame += 0.005 * time;
+		if (currentFrame > 3) currentFrame -= 3;
+
+		if (dx > 0) sprite.setTextureRect(IntRect(63 * int(currentFrame), 196, 60, 60));
+		if (dx < 0) sprite.setTextureRect(IntRect(69 * int(currentFrame), 130, 60, 60));
+		//что-то сделать с анимацией прыжка
+		if (dy > 0) sprite.setTextureRect(IntRect(60 * int(currentFrame), 60, 60, 60));
+
+
+		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
+
+		dx = 0;
+	}
+
+
+	// collision
+	void Collision(int dir)
+	{
+		for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
+			for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++)
+			{
+				if (TileMap[i][j] == 'B')
+				{
+					if ((dx > 0) && (dir == 0)) rect.left = j * 32 - rect.width;
+					if ((dx < 0) && (dir == 0)) rect.left = j * 32 + 32;
+					if ((dy > 0) && (dir == 1)) { rect.top = i * 32 - rect.height;  dy = 0;   onGround = true; }
+					if ((dy < 0) && (dir == 1)) { rect.top = i * 32 + 32;   dy = 0; }
+				}
+
+				if (TileMap[i][j] == '0')
+				{
+					TileMap[i][j] = ' ';
+				}
+
+			}
+
+	}
+};
